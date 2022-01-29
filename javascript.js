ccb78052048492640f9bedd71f673c006e2d5715
andrehebra <andrehebra@gmail.com>
2022-01-29 11:53:21 -0500

    simple javascript variables, math, and console.log

--- javascript.js
+++ javascript.js
@@ -1,4 +1,15 @@
-let message;
-message = 'hello';
+console.log(23 + 97);
 
-alert(message);
+console.log (25+1+53+234+234+3);
+
+console.log((4+6+9) / 77);
+
+let a = 10;
+
+console.log('a is equal to ' + a);
+
+console.log('9 * a is equal to ' + a*9);
+
+let b = 7 * a;
+
+console.log(b);
