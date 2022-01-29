4b036323d4664cbb9822bd5300fc4c362caf5bf1
andrehebra <andrehebra@gmail.com>
2022-01-29 11:55:18 -0500

    simple console practice

--- javascript.js
+++ javascript.js
@@ -1,15 +1,6 @@
-console.log(23 + 97);
+const max = 57;
 
-console.log (25+1+53+234+234+3);
+const actual = max-13
 
-console.log((4+6+9) / 77);
+const percentage = actual / max;
 
-let a = 10;
-
-console.log('a is equal to ' + a);
-
-console.log('9 * a is equal to ' + a*9);
-
-let b = 7 * a;
-
-console.log(b);
